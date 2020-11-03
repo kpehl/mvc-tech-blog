f513e79229bcae8896bdfa4534440dd57ca6d8ac
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-11-03 10:03:58 -0500

    modified homepage.handlebars and home-routes.js to get and display all the blog posts on the home page; modified stylesheet to eliminate list markers

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,48 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Post.findAll({
+        // Query configuration
+        // From the Post table, include the post ID, URL, title, and the timestamp from post creation
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+          ],
+        // Order the posts from most recent to least
+        order: [[ 'created_at', 'DESC']],
+        // From the User table, include the post creator's user name
+        // From the Comment table, include all comments
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    // render the posts
+    .then(dbPostData => {
+        // create an array for the posts, using the get method to trim extra sequelize object data out
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass the posts into the homepage template
+      res.render('homepage', {posts});
+    })
+    // if there was a server error, return the error
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
