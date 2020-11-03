410e5eafc532347e6e143bf7c9625a3f968ee457
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-11-03 10:53:06 -0500

    added single-post.handlebars and query in home-routes to render the requested single post; removed comment count span from singlel-post.handlebars, since all comments will be displayed on this page in the template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// Render the home page
 router.get('/', (req, res) => {
     Post.findAll({
         // Query configuration
@@ -33,7 +34,7 @@ router.get('/', (req, res) => {
     })
     // render the posts
     .then(dbPostData => {
-        // create an array for the posts, using the get method to trim extra sequelize object data out
+      // create an array for the posts, using the get method to trim extra sequelize object data out
       const posts = dbPostData.map(post => post.get({ plain: true }));
       // pass the posts into the homepage template
       res.render('homepage', {posts});
@@ -44,5 +45,52 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// Render the single post page
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        // specify the post id parameter in the query
+        id: req.params.id
+      },
+      // Query configuration, as with the get all posts route
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // if no post by that id exists, return an error
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        // serialize the post data, removing extra sequelize meta data
+        const post = dbPostData.get({ plain: true });
+        // pass the posts into the homepage template
+        res.render('single-post', {post});
+      })
+      .catch(err => {
+        // if a server error occured, return an error
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
